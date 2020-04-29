0ee856598dcc64b90885acc873a6eeb28d388c43
Anshul <samgalacestyle01@gmail.com>
2020-04-30 03:18:49 +0530

    Created a video for simple print statements in Python.

---
+++ python101.py
@@ -0,0 +1,16 @@
+# lets write a simple python code to print a few statements
+# Assumption: you already have the local environment setup for python
+
+# the obvious
+print('Hello World!')
+
+print('Hello Again')
+print('I like typing this.')
+print('This is fun !!')
+print('Yay! Its printing !!')
+
+# Cool stuff, right!
+# This is jist for someone who has to start from scratch and wanted to straightaway 
+# get into writing code rather than going through tons of books and material
+
+# thanks for watching!
