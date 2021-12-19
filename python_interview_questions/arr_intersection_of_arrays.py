1f4b859964ef3ce66beb323d4810d5ac3e1f3784
Anshul Jain <samgalacestyle01@gmail.com>
2021-12-19 23:30:10 +0530

    Completed 02_pythonIQ_arr_intersection_of_arrays

---
+++ python_interview_questions/arr_intersection_of_arrays.py
@@ -0,0 +1,36 @@
+'''
+Given two integer arrays nums1 and nums2, return an array of their intersection. Each element in the result must appear as many times as it shows in both arrays and you may return the result in any order.
+
+Example 1:
+Input: nums1 = [1,2,2,1], nums2 = [2,2]
+Output: [2,2]
+
+Example 2:
+Input: nums1 = [4,9,5], nums2 = [9,4,9,8,4]
+Output: [4,9]
+Explanation: [9,4] is also accepted.
+ 
+
+Constraints:
+
+1 <= nums1.length, nums2.length <= 1000
+0 <= nums1[i], nums2[i] <= 1000
+'''
+def find_intersection(nums1, nums2):
+    len1 = len(nums1)
+    len2 = len(nums2)
+
+    intersect = []
+
+    if len1 < len2:
+        for val in nums1:
+            if val in nums2:
+                intersect.append(val)
+                nums2.remove(val)
+    else:
+        for val in nums2:
+            if val in nums1:
+                intersect.append(val)
+                nums1.remove(val)
+
+    return intersect
